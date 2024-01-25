b9a1cac6ddbd1c00b02e91c374d32bd8905ebb02
Wolfe Weeks <wolfesevenweeks@gmail.com>
2024-01-25 06:33:34 -0600

    Update workspace file and refactor base_page.dart

---
+++ lib/core/auth/ui/widgets/auth_gate.dart
@@ -0,0 +1,40 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+
+import '../../data/repository/firebase_auth_repo.dart';
+
+class AuthGate extends ConsumerWidget {
+  final Widget child;
+
+  const AuthGate({required this.child, super.key});
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    return StreamBuilder<User?>(
+      stream: FirebaseAuthRepo.instance.authStateChanges(),
+      builder: (context, snapshot) {
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return const Center(
+            child: CircularProgressIndicator(),
+          );
+        }
+
+        if (snapshot.hasError) {
+          return const Center(
+            child: Text('Something went wrong'),
+          );
+        }
+
+        if (snapshot.connectionState == ConnectionState.active) {
+          if (snapshot.data == null) {
+            Future.microtask(() => context.go('/sign_in'));
+          }
+        }
+
+        return child;
+      },
+    );
+  }
+}
--- lib/core/ui/screens/base_page.dart
+++ lib/core/ui/screens/base_page.dart
@@ -1,9 +1,7 @@
-import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
-import 'package:go_router/go_router.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
-import '../../auth/data/repository/firebase_auth_repo.dart';
+import '../../auth/ui/widgets/auth_gate.dart';
 
 class BasePage extends ConsumerWidget {
   final Widget body;
@@ -13,30 +11,9 @@ class BasePage extends ConsumerWidget {
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     return Scaffold(
-      body: SafeArea(
-        child: StreamBuilder<User?>(
-          stream: FirebaseAuthRepo.instance.authStateChanges(),
-          builder: (context, snapshot) {
-            if (snapshot.connectionState == ConnectionState.waiting) {
-              return const Center(
-                child: CircularProgressIndicator(),
-              );
-            }
-
-            if (snapshot.hasError) {
-              return const Center(
-                child: Text('Something went wrong'),
-              );
-            }
-
-            if (snapshot.connectionState == ConnectionState.active) {
-              if (snapshot.data == null) {
-                Future.microtask(() => context.go('/sign_in'));
-              }
-            }
-
-            return body;
-          },
+      body: AuthGate(
+        child: SafeArea(
+          child: body,
         ),
       ),
     );
